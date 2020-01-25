8dcccc59eac61ccc1f331040245f460be0cef1d3
Samarth Patel <samarth.patel.22@hotmail.com>
2020-01-25 18:09:25 -0500

    Added Session files

---
+++ ui/src/components/Session.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../css/Session.css'
+
+class Session extends React.Component {
+
+    /**
+     * 
+     * session title
+     * list of questions 
+     * 
+     */
+    constructor(props) {
+
+    }
+
+    render() {
+        return (
+            <div className>
+
+            </div>
+        )
+    }
+}
+
+export default Session;
