93497ac279f89594629675f16b0b54f6f8ac7bde
Dwip Patel <dwipatel00@hotmail.com>
2020-01-25 18:33:39 -0500

    title component done

---
+++ ui/src/components/Title.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import '../css/Title.css'
+
+export default class Title extends Component {
+    render() {
+        return (
+            <div className="banner">
+                Welcome to Your Healthier Life.
+            </div>
+        )
+    }
+}
